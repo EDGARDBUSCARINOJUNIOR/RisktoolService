aac2300a89dbda78f8545d4e10b8488da74a5538
edbr <edgard.buscarino@gft.com>
2022-07-31 11:34:08 -0300

    First commit

---
+++ RiskToolService/Worker.cs
@@ -0,0 +1,39 @@
+namespace RiskToolService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                File.WriteAllText("C:\\temp\\Log.txt", DateTimeOffset.Now.ToString());
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
